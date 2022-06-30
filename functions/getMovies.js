9500bf0f434c3b585a74130b4e1fdfdc2c79d60d
Yi-Chang Lin <kiraskywing@gmail.com>
2022-06-30 14:44:53 +0800

    first try to read movie data

---
+++ functions/getMovies.js
@@ -0,0 +1,43 @@
+const fetch = require('node-fetch')
+
+exports.handler = async function () {
+    const url = process.env.ASTRA_GRAPHQL_ENDPOINT
+    const query = `
+        query {
+            movies_by_genre (
+                value: { genre: "Sci-Fi" },
+                orderBy: [year_DESC]
+            ) {
+                values {
+                    year,
+                    title,
+                    duration,
+                    synopsis,
+                    thumbnail
+                }
+            }
+        }
+    `  
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "x-cassandra-token": process.env.ASTRA_DB_APPLICATION_TOKEN
+        },
+        body: JSON.stringify({ query })
+    })
+
+    try {
+        const responseBody = await response.json()
+        return {
+            statusCode: 200,
+            body: JSON.stringify(responseBody)
+        }
+    } catch (e) {
+        console.log(e)
+        return {
+            statusCode: 500,
+            body: JSON.stringify(e)
+        }
+    }
+}
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,6 +1,31 @@
+import { useEffect, useState } from 'react'
+import Card from './Card'
+
 const Section = ({ genre }) => {
+    const [movies, setMovies] = useState(null)
+
+    const fetchData = async() => {
+        const response = await fetch("/.netlify/functions/getMovies")
+        const responseBody = await response.json()
+        // console.log(responseBody.data.movies_by_genre.values)
+        setMovies(responseBody.data.movies_by_genre.values)
+    }
+    
+    useEffect(() => {
+        fetchData()
+    }, [])
+    
     return (
-        <div>{genre}</div>
+        <>
+            <div>{genre}</div>
+            {movies && (
+                <div className="movie-section">
+                    {movies.map((movie, index) => (
+                        <Card key={index} movie={movie}/>
+                    ))}
+                </div>
+            )}
+        </>
     )
 }
 
