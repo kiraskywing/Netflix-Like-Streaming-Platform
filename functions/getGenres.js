9894a124a9f058f5eef318d1f2e9542ca75231ae
Yi-Chang Lin <kiraskywing@gmail.com>
2022-06-30 13:44:54 +0800

    first commit

---
+++ functions/getGenres.js
@@ -0,0 +1,38 @@
+const fetch = require('node-fetch')
+
+exports.handler = async function () {
+    const url = process.env.ASTRA_GRAPHQL_ENDPOINT
+    const query = `
+        query getAllGenres {
+            reference_list (
+                value: { label: "genre" }
+            ) {
+                values {
+                value
+                }
+            }
+        }
+    `  
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "x-cassandra-token": process.env.ASTRA_DB_APPLICATION_TOKEN
+        },
+        body: JSON.stringify({ query })
+    })
+
+    try {
+        const responseBody = await response.json()
+        return {
+            statusCode: 200,
+            body: JSON.stringify(responseBody)
+        }
+    } catch (e) {
+        console.log(e)
+        return {
+            statusCode: 500,
+            body: JSON.stringify(e)
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [genres, setGenres] = useState(null)
+  
+  const fetchData = async() => {
+    const response = await fetch("/.netlify/functions/getGenres")
+    const responseBody = await response.json()
+    console.log(responseBody.data.reference_list.values)
+    setGenres(responseBody.data.reference_list.values)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    </>
+  )
 }
 
 export default App;
