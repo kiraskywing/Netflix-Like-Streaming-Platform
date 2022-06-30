4874efacc525061922fd96028e79b5455d612a7b
Yi-Chang Lin <kiraskywing@gmail.com>
2022-06-30 19:49:17 +0800

    miner modify

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 const Card = ({movie}) => {
     
     return (
-        <div>
+        <div className="card">
             <h4>{movie.title}</h4>
             <h4>{movie.duration}</h4>
             <video className="video" controls>
