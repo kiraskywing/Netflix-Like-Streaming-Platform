7a10e538675da3c629278f7e3af14843f20d81ea
Yi-Chang Lin <kiraskywing@gmail.com>
2022-06-30 16:15:33 +0800

    dynamicly load more genres

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import './App.css'
 import Section from './components/Section'
 
 const App = () => {
+  const genreIncrement = 4
   const [genres, setGenres] = useState(null)
+  const [limit, setLimit] = useState(genreIncrement)
   
   const fetchData = async() => {
-    const response = await fetch("/.netlify/functions/getGenres")
+    const response = await fetch("/.netlify/functions/getGenres", {
+      method: "POST",
+      body: limit
+    })
     const responseBody = await response.json()
     // console.log(responseBody.data.reference_list.values)
     setGenres(responseBody.data.reference_list.values)
@@ -14,12 +19,17 @@ const App = () => {
 
   useEffect(() => {
     fetchData()
-  }, [])
+  }, [, limit])
 
   console.log(genres)
   return (
     <>
       {genres && (Object.values(genres).map((genre) => (<Section genre={genre.value}/>)))}
+      <div className="page-end"
+        onMouseEnter={() => {
+          setLimit(limit + genreIncrement)
+        }}
+      />
     </>
   )
 }
